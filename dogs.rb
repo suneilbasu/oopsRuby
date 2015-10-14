22ba30843375ac85e2aa7adef9a1cc1d9f7baaba
Suneil Basu <suneil@futureprogress.com>
2015-10-14 06:42:17 +0100

    oops demo in ruby

---
+++ dogs.rb
@@ -0,0 +1,17 @@
+class Dog
+	def initialize(breed,name)
+		@breed = breed
+		@name = name
+	end
+
+	def bark
+		puts 'Woof, Woof'
+	end
+	def display
+		puts "I am a #{@breed} breed and my name is #{@name}"
+	end
+end
+
+d1 = Dog.new('Whippet','Benzy')
+d1.bark
+d1.display
